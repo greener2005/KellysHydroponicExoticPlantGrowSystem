ade07d66996fc658a53d41d7624e661919a01286
greener <jgreen@intertech.com>
2016-12-01 14:57:44 -0600

    First Checkin

---
+++ KellysHydroponicExoticPlantGrowSystem/Controllers/PlantSensorsController.cs
@@ -0,0 +1,32 @@
+﻿using KellysHydroponicExoticPlantGrowSystem.Interfaces;
+using Microsoft.Practices.ServiceLocation;
+using Restup.Webserver.Attributes;
+using Restup.Webserver.Models.Schemas;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace KellysHydroponicExoticPlantGrowSystem.Controllers
+{
+    public class PlantSensorsController
+    {
+        private readonly IPlantMonitoringService _plantMonitoringService;
+
+        public PlantSensorsController()
+        {
+            _plantMonitoringService = ServiceLocator.Current.GetInstance<IPlantMonitoringService>();
+            InitPlantServices().ContinueWith(t => Debug.WriteLine(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        private async Task InitPlantServices()
+        {
+            Debug.WriteLine("test");
+        }
+
+        [UriFormat("/GetPlantSensorData")]
+        public async Task<GetResponse> GetPlantSensorData()
+        {
+            return null;
+        }
+    }
+}
--- KellysHydroponicExoticPlantGrowSystem/Views/MainPage.xaml.cs
+++ KellysHydroponicExoticPlantGrowSystem/Views/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace KellysHydroponicExoticPlantGrowSystem
+namespace KellysHydroponicExoticPlantGrowSystem.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,6 +25,11 @@ namespace KellysHydroponicExoticPlantGrowSystem
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
